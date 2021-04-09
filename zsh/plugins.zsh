05c7ce9e831581d7b76d76a785459c3776f8f472
kult0922 <38486600+kult0922@users.noreply.github.com>
2021-04-09 10:37:36 +0900

    bat command

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,1 +1,12 @@
+# 見た目のカスタマイズ
 zinit ice depth=1; zinit light romkatv/powerlevel10k
+# コマンドラインのハイライト
+zinit light zdharma/fast-syntax-highlighting
+
+# bat command
+zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
+zinit light sharkdp/bat
+# alias
+if builtin command -v bat > /dev/null; then
+  alias cat="bat"
+fi
