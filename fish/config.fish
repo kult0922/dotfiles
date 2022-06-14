6964cfc8c3d35792d047adbeb0bfd6d95950f60d
kult0922 <38486600+kult0922@users.noreply.github.com>
2022-06-15 01:54:08 +0900

    add shortcut for peco

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,7 @@ export LSCOLORS=cxfxcxdxbxegedabagacad
 if status is-interactive
     eval (/opt/homebrew/bin/brew shellenv)
 end
+
+function fish_user_key_bindings
+    bind \cr peco_select_history
+end
