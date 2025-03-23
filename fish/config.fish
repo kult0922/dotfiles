c53aa5951bcc216e50ff680ae77ea8bf6874bc83
kult0922 <38486600+kult0922@users.noreply.github.com>
2025-03-23 14:05:34 +0900

    for go

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,12 @@ end
 function fish_user_key_bindings
     bind \cr peco_select_history
 end
+
+# for go
+set -x GOENV_ROOT $HOME/.goenv
+set -x PATH $GOENV_ROOT/bin $PATH
+eval (goenv init - | source)
+set -x PATH $GOPATH/bin $PATH
+
+# for starship prompt
+starship init fish | source
