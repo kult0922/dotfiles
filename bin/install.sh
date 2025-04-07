7f2cd5d23ef7d9f256534fd52b0a0953ff200eec
kult0922 <38486600+kult0922@users.noreply.github.com>
2025-04-07 23:40:02 +0900

    fix link

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
-ln -s ~/dotfiles/fish ~/.config/fish
-ln -s ~/dotfiles/wezterm ~/.config/wezterm
+ln -s ~/dotfiles/fish ~/.config/
+ln -s ~/dotfiles/wezterm ~/.config/
